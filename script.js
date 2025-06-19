e22efbfd05da025e1af57df0b3278ae430351bcf
Nour-2101 <nourelhoudagaraoui@gmail.com>
2025-06-19 23:50:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+
